9076d20525d7d915fef584d22427af0650089e64
Stephen <stephen.j.tong@gmail.com>
2011-06-25 18:05:40 +0800

    init

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails'
+gem 'mongoid'
+gem 'bson_ext'
+gem 'haml'
+#gem 'gravtastic'
+gem 'jquery-rails'
+gem 'will_paginate'
+
+gem 'devise'
+#
+gem 'oauth'
+gem 'omniauth', '0.2.0'
+gem "omniauth_china", "0.0.6"
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Src2::Application.config.secret_token = '4e4c93d53f2b764fff74cd8854b5c17b0ada58b814ca929b609db4580546f41c7beb7904dbd1d21227497a0265f7ba2569250b4090249fbba656126d99222633'
